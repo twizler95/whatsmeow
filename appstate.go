424b73de91dc6d5a9e14805ca362b3b3b90cd9f4
Nick (M2) <nicktee89@gmail.com>
2025-11-23 11:26:06 +0800

    Update appstate.go
    update DecodePatche to skip lthash validation

--- appstate.go
+++ appstate.go
@@ -60,7 +60,7 @@ func (cli *Client) FetchAppState(ctx context.Context, name appstate.WAPatchName,
 		}
 		hasMore = patches.HasMorePatches
 
-		mutations, newState, err := cli.appStateProc.DecodePatches(ctx, patches, state, true)
+		mutations, newState, err := cli.appStateProc.DecodePatches(ctx, patches, state, false)
 		if err != nil {
 			if errors.Is(err, appstate.ErrKeyNotFound) {
 				go cli.requestMissingAppStateKeys(context.WithoutCancel(ctx), patches)
